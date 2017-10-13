5ef2c61e4d809d6bb1d14fd5c41c574a9bab0ac7
Shane Williamson <williamson.shane.a@gmail.com>
2017-10-12 20:14:15 -0600

    Added beggining of config

---
+++ a1/model/config.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"sync"
+)
+
+// Config is a signleton which should be accessed/initialized only by the GetInstance function, it houses the program configuration
+type Config struct {
+	ClientConfig *ClientConfig
+}
+
+var instance *Config
+var once sync.Once
+
+// GetInstance returns a singleton instance of the program configuration
+func GetInstance() *Config {
+	once.Do(func() {
+		instance = &Config{}
+	})
+	return instance
+}
+
+// ClientConfig houses the configurations for the client portion of the program
+type ClientConfig struct {
+	MessagePort uint16
+}
+
+// ServerConfig houses the configurations for the client portion of the program
+type ServerConfig struct {
+	Address string
+	Port    uint16
+}
